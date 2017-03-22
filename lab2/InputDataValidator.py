70b38b9689835ce518b968f17fec0508ca88109b
Piotr Chołody <piotr.cholody123@gmail.com>
2017-03-22 13:50:52 +0100

    travis test

--- lab2/InputDataValidator.py
+++ lab2/InputDataValidator.py
@@ -13,27 +13,6 @@ class AbstractValidator(object):
 class InputDataValidator(AbstractValidator):
 
     @staticmethod
-    def validate(self, first, second, equation):
-        if equation != "derivative":
-            if not self._is_number(first) or not self._is_number(second):
-                raise Exceptions.NotANumber
-        if equation == "logarithm":
-            if not self._is_positive(first):
-                raise Exceptions.NotAPositiveNumber
-            if not self._is_log_base_valid(second):
-                raise Exceptions.NotValidLogarithmBase
-        if equation == "derivative":
-            if not self._is_string(first):
-                raise Exceptions.NotAFunction
-            if not self._is_integer(second):
-                raise Exceptions.NotAnInteger
-            if not self._is_positive(second):
-                raise Exceptions.NotAPositiveNumber
-        if equation == "divide":
-            if self._is_zero(second):
-                raise Exceptions.DivideByZero
-
-    @staticmethod
     def _is_number(number):
         return isinstance(number, (int, float))
 
@@ -56,6 +35,29 @@ class InputDataValidator(AbstractValidator):
     @staticmethod
     def _is_integer(number):
         return isinstance(number, int)
+
+    @staticmethod
+    def validate(self, first, second, equation):
+        if equation != "derivative":
+            if not self._is_number(first) or not self._is_number(second):
+                raise Exceptions.NotANumber
+        if equation == "logarithm":
+            if not self._is_positive(first):
+                raise Exceptions.NotAPositiveNumber
+            if not self._is_log_base_valid(second):
+                raise Exceptions.NotValidLogarithmBase
+        if equation == "derivative":
+            if not self._is_string(first):
+                raise Exceptions.NotAFunction
+            if not self._is_integer(second):
+                raise Exceptions.NotAnInteger
+            if not self._is_positive(second):
+                raise Exceptions.NotAPositiveNumber
+        if equation == "divide":
+            if self._is_zero(second):
+                raise Exceptions.DivideByZero
+
+
 
 
 

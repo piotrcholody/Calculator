d1c9940d6b0f918b419ec6f621674098c4791e51
piotrcholody <piotr.cholody123@gmail.com>
2017-03-22 11:38:46 +0100

    Add files via upload

---
+++ lab2/Exceptions.py
@@ -0,0 +1,22 @@
+class NotANumber(Exception):
+    pass
+
+
+class NotAnInteger(Exception):
+    pass
+
+
+class NotAPositiveNumber(Exception):
+    pass
+
+
+class DivideByZero(Exception):
+    pass
+
+
+class NotValidLogarithmBase(Exception):
+    pass
+
+
+class NotAFunction(Exception):
+    pass
---
+++ lab2/InputDataValidator.py
@@ -0,0 +1,65 @@
+import Exceptions
+import abc
+
+
+class AbstractValidator(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def validate(self, first, second, equation):
+        pass
+
+
+class InputDataValidator(AbstractValidator):
+
+    @staticmethod
+    def validate(self, first, second, equation):
+
+        if equation != "derivative":
+            if not self._is_number(first) or not self._is_number(second):
+                raise Exceptions.NotANumber
+
+        if equation == "logarithm":
+            if not self._is_positive(first):
+                raise Exceptions.NotAPositiveNumber
+            if not self._is_log_base_valid(second):
+                raise Exceptions.NotValidLogarithmBase
+
+        if equation == "derivative":
+            if not self._is_string(first):
+                raise Exceptions.NotAFunction
+            if not self._is_integer(second):
+                raise Exceptions.NotAnInteger
+            if not self._is_positive(second):
+                raise Exceptions.NotAPositiveNumber
+
+        if equation == "divide":
+            if self._is_zero(second):
+                raise Exceptions.DivideByZero
+
+    @staticmethod
+    def _is_number(number):
+        return isinstance(number, (int, float))
+
+    @staticmethod
+    def _is_string(string):
+        return isinstance(string, str)
+
+    @staticmethod
+    def _is_zero(number):
+        return number == 0
+
+    @staticmethod
+    def _is_log_base_valid(number):
+        return number > 0 and number != 1
+
+    @staticmethod
+    def _is_positive(number):
+        return number > 0
+
+    @staticmethod
+    def _is_integer(number):
+        return isinstance(number, int)
+
+
+

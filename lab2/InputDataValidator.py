6dc996b018db178dfd2c2a8152520b250d92f182
Piotr Chołody <piotr.cholody123@gmail.com>
2017-03-22 13:47:16 +0100

    travvis test

--- lab2/InputDataValidator.py
+++ lab2/InputDataValidator.py
@@ -14,17 +14,14 @@ class InputDataValidator(AbstractValidator):
 
     @staticmethod
     def validate(self, first, second, equation):
-
         if equation != "derivative":
             if not self._is_number(first) or not self._is_number(second):
                 raise Exceptions.NotANumber
-
         if equation == "logarithm":
             if not self._is_positive(first):
                 raise Exceptions.NotAPositiveNumber
             if not self._is_log_base_valid(second):
                 raise Exceptions.NotValidLogarithmBase
-
         if equation == "derivative":
             if not self._is_string(first):
                 raise Exceptions.NotAFunction
@@ -32,7 +29,6 @@ class InputDataValidator(AbstractValidator):
                 raise Exceptions.NotAnInteger
             if not self._is_positive(second):
                 raise Exceptions.NotAPositiveNumber
-
         if equation == "divide":
             if self._is_zero(second):
                 raise Exceptions.DivideByZero
